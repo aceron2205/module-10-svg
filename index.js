d0bba4f1ce238b74b3a6e06f963c92de1ce8ad1d
Andrea Ceron <andreacmz1996@gmail.com>
2023-08-17 02:44:55 +0000

    changing resolution to 300x200

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 
 const generateSVG = ({ color, shape, text }) => {
   let svgContent = '';
-  const width = 200;
+  const width = 300;
   const height = 200;
   const centerX = width / 2;
   const centerY = height / 2;
@@ -13,10 +13,10 @@ const generateSVG = ({ color, shape, text }) => {
       svgContent += `<circle cx="${centerX}" cy="${centerY}" r="80" fill="${color}"/>`;
       break;
     case 'rectangle':
-      svgContent += `<rect x="25" y="50" width="150" height="100" fill="${color}"/>`;
+      svgContent += `<rect x="50" y="50" width="200" height="100" fill="${color}"/>`;
       break;
     case 'triangle':
-      svgContent += `<polygon points="100,30 190,170 10,170" fill="${color}" />`;
+      svgContent += `<polygon points="150,30 290,170 10,170" fill="${color}" />`;
       break;
   }
 
